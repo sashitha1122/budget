a3b4e80a99513f3a6f26b99ddd0e3d7fe4727838
Sashitha <user@DESKTOP-6H9VGAS>
2024-10-14 13:25:00 +0530

    create budgetform

---
+++ src/components/AddBudgetForm.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { Form } from 'react-router-dom';
+
+const AddBudgetForm = () => {
+    return (
+      <div className="d-flex justify-content-center">
+           <div className="alert alert-success w-50" style={{ height: '400px' }} role="alert">
+            <h2 className='text-center'>  Create Budget</h2>
+            <Form method="post" class="">
+               <div className="">
+                <label className="fs-5 mt-3" htmlFor="newBudget">Budget Name </label>
+                <input type="text" class="form-control mt-3" name="newBudget" id="newBudget" placeholder="e.g., Groceries" required />
+               </div>
+               <div class="mt-3">
+                <label className="fs-5 mt-3" htmlFor="newBudgetAmount">Amount </label>
+                <input type="number" class="form-control mt-3"  name="newBudgetAmount" id="newBudgetAmount" placeholder="e.g., 4000 LKR" required inputMode="decimal" />
+               </div>
+               <div className="mt-3 pt-3">
+               <button type="submit" className=" mt-3 btn btn-dark">Create Budget</button>   
+               </div> 
+            </Form>
+          </div>
+      </div>  
+    );
+};
+
+export default AddBudgetForm;
